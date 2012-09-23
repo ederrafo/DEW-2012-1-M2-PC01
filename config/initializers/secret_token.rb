5d85c2466431e657d1352fc8ff5039c0b2725a85
eder sony <ederrafo@gmail.com>
2012-09-22 22:25:49 -0500

    inicio de la pc01

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pc01::Application.config.secret_token = 'a7455e814c6d9e828e187272a484dee39dc40c28ecf353b88be20e8bacd2e55ce974c015750bb283476454295a304231b9b1ac51be82447baf9c87c3ad42b7c6'
