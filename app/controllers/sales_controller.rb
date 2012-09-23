06f51859b7305676d2347d9f196cfafb1e5109ea
eder sony <ederrafo@gmail.com>
2012-09-23 17:59:43 -0500

    Termino correcto de la practica

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -43,10 +43,55 @@ class SalesController < ApplicationController
   # POST /sales
   # POST /sales.json
   def create
-    @sale = Sale.new(params[:sale])
+    #var resultado=0
+    #@ficha = Ficha.new(params[:ficha])
+    #@ficha.saldo = @ficha.abono + @ficha.cargo
 
+    @sale = Sale.new(params[:sale])
+    #abort(params[:sale][:product_ids])
+    #params[:sale][:product_ids].each_with_index do |elemento, indice|
+     # "Aca hacemos lo que queremos con el elemento #{elemento} y con el indice #{indice}"
+    #@product = Product.find(elemento)
+    #@elemento1 =  2+@product.price 
+    #puts "->>"+elemento
+    #abort(@product.title)
+    #resultado = resultado +elemento1
+    #abort(elemento1)
+    #end
+@elemento1 = 0
+#array = [1, "dos", 3]
+params[:sale][:product_ids].each { |item|
+ puts "item :"+item 
+ if item != ""
+  puts "No es vacio"
+  @product = Product.find(item)
+  @elemento1 =  @elemento1 + @product.price 
+ 
+ end
+ #@sums= 3 + @product.price
+ #puts "====>>>>"
+  }
+
+    #puts @
+    puts @elemento1
+ params[:sale][:total] = @elemento1
+    
+    #abort("final");
     respond_to do |format|
       if @sale.save
+        @sale = Sale.find(@sale.id)
+        @sale.update_attributes(:customer_id => session[:id], :total => @elemento1)
+        
+       # @wise = Wise.new(:website => params[:wise][:website],
+       #           :apellation => params[:wise][:apellation],
+       #           :banck_account => params[:wise][:banck_account],
+       #           :bank => params[:wise][:bank],
+       #           :user_id => @user.id,
+       #           :summary => params[:wise][:summary])
+
+#t.integer  "customer_id"
+#t.decimal  "total"
+
         format.html { redirect_to @sale, notice: 'Sale was successfully created.' }
         format.json { render json: @sale, status: :created, location: @sale }
       else
